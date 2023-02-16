14e1acadbd7c7b994fd02320cd13b6d710135b92
GACH <germanach@educastur.org>
2023-02-16 13:12:55 +0100

    Initial project CollectionViewDemo

---
+++ CollectionViewDemo/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using CollectionViewDemo.MVVM.Views;
+
+namespace CollectionViewDemo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            // MainPage = new DataView();
+            // MainPage = new LayoutView();
+            // MainPage = new EmptyView();
+            MainPage = new ProductsView();
+        }
+    }
+}
---
+++ CollectionViewDemo/MVVM/Views/ProductsView.xaml.cs
@@ -0,0 +1,34 @@
+using CollectionViewDemo.MVVM.ViewModels;
+using System.Diagnostics;
+
+namespace CollectionViewDemo.MVVM.Views;
+
+public partial class ProductsView : ContentPage
+{
+	public ProductsView()
+	{
+		InitializeComponent();
+		BindingContext = new ProductsViewModel();
+	}
+
+    private void CollectionView_Scrolled(object sender, ItemsViewScrolledEventArgs e)
+    {
+		Debug.WriteLine("-------------------------------------------");
+		Debug.WriteLine("HorizontalDelta:" + e.HorizontalDelta);
+		Debug.WriteLine("VerticalDelta: " + e.VerticalDelta);
+		Debug.WriteLine("HorizontalOffset: " + e.HorizontalOffset);
+		Debug.WriteLine("VerticalOffset: " + e.VerticalOffset);
+		Debug.WriteLine("FirstVisibleItemIndex: " + e.FirstVisibleItemIndex);
+		Debug.WriteLine("CenterItemIndex: " + e.CenterItemIndex);
+		Debug.WriteLine("LastVisibleItemIndex: " + e.LastVisibleItemIndex);
+		Debug.WriteLine("-------------------------------------------");
+    }
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+		// collectionView.ScrollTo(10);
+		var viewModel = BindingContext as ProductsViewModel;
+		var product = viewModel.Products.SelectMany(p => p).FirstOrDefault(p => p.Id == 10);
+		collectionView.ScrollTo(product);
+    }
+}
